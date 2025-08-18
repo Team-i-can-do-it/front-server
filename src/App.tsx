ca9dde4dc84494a49a5a3fd46c1524bfc1ad33de
김호영 <happy3sk@naver.com>
2025-08-18 18:09:52 +0900

    docs: 기본 css 주석처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
+import DefaultLayout from './components/layout/DefaultLayout';
 
 function App() {
   return (
-    <>
-      <div className="bg-purple-50 w-40 h-40">Hello world</div>
-    </>
+    <DefaultLayout>
+      <div className="flex justify-center items-center bg-purple-50 w-40 h-40">
+        Hello world
+      </div>
+    </DefaultLayout>
   );
 }
 
