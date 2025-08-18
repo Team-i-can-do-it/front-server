eec3cca6855082b02dfd57942ee13548dd9aa312
김호영 <happy3sk@naver.com>
2025-08-18 17:17:59 +0900

    feat: design system 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-  return <>Hello world</>;
+  return (
+    <>
+      <div className="bg-purple-50 w-40 h-40">Hello world</div>
+    </>
+  );
 }
 
 export default App;
