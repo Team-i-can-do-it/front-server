4a9be0f0258ac6af7225d87534d204f472f80c46
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2025-08-25 17:53:12 +0900

    Feature/design system 적용
    * feat: Prototype 초기 design system 설정 반영
    
    * feat: 레이아웃 수정 및, design system과 관련 없는 파일 제거
    
    * feat: design-system color 설정
    
    * style: 시멘틱 태그 색상 정의
    
    * style: Typography 설정 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import './App.css';
+import DefaultLayout from './components/layout/DefaultLayout';
 
 function App() {
-  return <>Hello world</>;
+  return (
+    <DefaultLayout>
+      <div className="flex justify-center items-center bg-violet-50 bg w-40 h-40">
+        Hello world
+      </div>
+
+      <div className="items-center justify-center flex bg-amber-200 w-40 h-40 text-white-base">
+        test
+      </div>
+    </DefaultLayout>
+  );
 }
 
 export default App;
