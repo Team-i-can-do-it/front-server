22bfcec54a014a2ee43715c3e9a1f8d6e8a8f1fe
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2025-08-27 14:49:31 +0900

    feat: Header 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import './App.css';
-import DefaultLayout from '@_components/layout/DefaultLayout';
+import DefaultLayout from '@_layout/DefaultLayout';
 import DesignTest from '@_page/DesignTest';
-import { Routes } from 'react-router-dom';
-import { Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Header from '@_layout/Header';
 
 function App() {
   return (
     <Routes>
-      <Route element={<DefaultLayout />}>
+      <Route
+        element={
+          <DefaultLayout
+            header={
+              <Header
+                title="스타일 테스트"
+                showBack
+                backTo="/"
+                showClose={true}
+              />
+            }
+          />
+        }
+      >
         <Route path="/style" element={<DesignTest />} />
       </Route>
     </Routes>
