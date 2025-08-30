7291eabc0c29990d5459aeb3a96696d0d151c19e
김호영 <happy3sk@naver.com>
2025-08-30 18:09:10 +0900

    feat: stye 페이지에서 lottie 기능 체크

--- src/page/DesignTest.tsx
+++ src/page/DesignTest.tsx
@@ -1,11 +1,42 @@
 import VioletTag, { GrayTag, WhiteTextTag } from '@_components/common/Tag';
+import Lottie from 'react-lottie-player';
+import logo from '@_icons/logo/logo.svg';
+import { useEffect, useState } from 'react';
 
 export default function DesignTest() {
+  const [animationData, setAnimationData] = useState<object>();
+  useEffect(() => {
+    import('../assets/characters/dog.json').then((data) => {
+      setAnimationData(data.default); // dynamic import일 땐 .default 필요
+    });
+  }, []);
+
+  if (!animationData)
+    return (
+      <div className="w-fll h-[201px] md:h-[467px] lg:h-[336px] flex items-center justify-center">
+        <img
+          src={logo}
+          alt="loading"
+          width={50}
+          height={50}
+          className="animate-spin"
+        />
+      </div>
+    );
   return (
     <>
       <GrayTag label="# 감정형" />
       <VioletTag label="보라색" />
       <WhiteTextTag label="보라색" />
+
+      <div className="w-[173.5px] h-[216.3px]">
+        <Lottie
+          loop
+          animationData={animationData}
+          play
+          style={{ width: '100%', height: '100%' }}
+        />
+      </div>
     </>
   );
 }
