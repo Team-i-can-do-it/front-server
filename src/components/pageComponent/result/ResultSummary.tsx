dd78a6fbe3c21fccd74b9ba0bc5d9055cbc0d512
김호영 <happy3sk@naver.com>
2025-09-05 16:35:51 +0900

    feat: 디자인 변경 예정으로 우선 중단

--- src/components/pageComponent/result/ResultSummary.tsx
+++ src/components/pageComponent/result/ResultSummary.tsx
@@ -1,9 +1,15 @@
 import ScoreCard from './analysis.tsx/ScoreCard';
+import { useMyResult } from '@_hooks/useMyResult';
 
 export default function ResultSummary() {
+  const { data, isLoading, error } = useMyResult();
+
+  if (isLoading) return <ScoreCard isLoading />;
+  if (error) return <div className="px-6 text-red-500">점수 불러오기 실패</div>;
+
   return (
-    <>
-      <ScoreCard />
-    </>
+    <section className="space-y-6">
+      <ScoreCard name={data?.name} score={data?.score} />
+    </section>
   );
 }
